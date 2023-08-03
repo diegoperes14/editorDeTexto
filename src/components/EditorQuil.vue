8011b40b2f83b60c6e609556e54eb14c1e06a206
Diego Lima <diegoperes142@gmail.com>
2023-08-03 18:08:41 -0300

    Primeiro commit, onde adicionei o editor

---
+++ src/components/EditorQuil.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1>Editor</h1>
+      <div>
+        <quill-editor v-model="conteudo" theme="snow"></quill-editor>
+      </div>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import {QuillEditor} from '@vueup/vue-quill'
+  import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Editor',
+    components: {
+      QuillEditor,
+    },
+    data() {
+      return {
+        count: 1,
+        conteudo: '',
+      }
+    }
+  })
+</script>
---
+++ src/views/vue2editor.vue
@@ -0,0 +1,7 @@
+<template>
+  <editor-quil />
+</template>
+
+<script lang="ts" setup>
+  import EditorQuil from '@/components/EditorQuil.vue'
+</script>
